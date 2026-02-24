72a61132121962985ea63936f4493e4bec3be2c1
RanjithMichael <branjithmichael@gmail.com>
2026-02-24 23:18:04 +0530

    updated

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "https://bloggingplatformclient.netlify.app",
+  "https://bpclient.netlify.app",
 ];
 
 app.use(
