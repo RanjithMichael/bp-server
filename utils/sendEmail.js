8edba0ce1046f506a59b254992e6c5acce8c94fb
RanjithMichael <branjithmichael@gmail.com>
2026-03-16 22:48:58 +0530

    updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,7 @@ import SibApiV3Sdk from "sib-api-v3-sdk";
 
 const client = SibApiV3Sdk.ApiClient.instance;
 const apiKey = client.authentications["api-key"];
-apiKey.apiKey = process.env.BREVO_API_KEY; // make sure this is set in .env
+apiKey.apiKey = process.env.BREVO_API_KEY; 
 
 const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
 
